714071a3176f789669636a467bb41ec335cccc5a
Yogesh <yogesh@10decoders.in>
2018-04-23 20:18:46 +0530

    Changed the string to lowercase

--- index.js
+++ index.js
@@ -16,9 +16,6 @@ server.post('/IRCTC-find-trains', (req, res) => {
     var destination = req.body.result.parameters.destination;
     var date = req.body.result.parameters.date;
 
-    console.log("Source-->", source)
-    console.log("destination-->", destination)
-    console.log("date-->", date)
     var apiKey = 'ay8dlpzcb6';
     var srcStationCode, destStationCode;
 
@@ -28,15 +25,13 @@ server.post('/IRCTC-find-trains', (req, res) => {
   		} else {
   			var src = JSON.parse(body).stations[0].code;
         srcStationCode = src;
-        console.log("srcStationCode-->", srcStationCode)
         request('https://api.railwayapi.com/v2/name-to-code/station/'+destination+'/apikey/'+apiKey+'/', function(error, responseFromAPI, body) {
       		if (error) {
       			console.log("ERR:", error);
       		} else {
       			var dest = JSON.parse(body).stations[0].code;
             destStationCode = dest;
-            console.log("destStationCode-->", destStationCode)
-            if(srcStationCode && destStationCode) {
+            if(srcStationCode && destStationCode && date) {
               var formattedDate = moment(date).format('DD-MM-YYYY');
               const reqUrl = 'https://api.railwayapi.com/v2/between/source/'+srcStationCode+'/dest/'+destStationCode+'/date/'+formattedDate+'/apikey/'+apiKey+'/';
               request(reqUrl, function(error, responseFromAPI, body) {
@@ -44,12 +39,11 @@ server.post('/IRCTC-find-trains', (req, res) => {
             			console.log("ERR:", error);
             		} else {
                   var trains = JSON.parse(body).trains;
-                  console.log("trains-->", trains)
                   if(trains) {
                     var trainsList = [];
               			for (var i = 0; i < trains.length; i++) {
                       var replacedStr = trains[i].name.replace(/EXP/g, "Express");
-                      var trainInfo = replacedStr + ' at ' + trains[i].src_departure_time;
+                      var trainInfo = replacedStr.toLowerCase() + ' at ' + trains[i].src_departure_time;
               				trainsList.push(trainInfo);
               			}
                     let dataToSend = "Available trains are " + trainsList.toString();
