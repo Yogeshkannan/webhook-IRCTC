580e99873a91d18bdd0a2b848305278abe5f97c9
Yogesh <yogesh@10decoders.in>
2018-04-25 14:44:34 +0530

    Condition changes

--- index.js
+++ index.js
@@ -23,15 +23,17 @@ server.post('/IRCTC-find-trains', (req, res) => {
   		if (error) {
   			console.log("ERR:", error);
   		} else {
-  			var src = JSON.parse(body).stations[0].code;
-        if(src) {
+  			var srcBody = JSON.parse(body);
+        if(srcBody && srcBody.stations && srcBody.stations.length >= 1) {
+          var src = srcBody.stations[0].code;
           srcStationCode = src;
           request('https://api.railwayapi.com/v2/name-to-code/station/'+destination+'/apikey/'+apiKey+'/', function(error, responseFromAPI, body) {
         		if (error) {
         			console.log("ERR:", error);
         		} else {
-        			var dest = JSON.parse(body).stations[0].code;
-              if(dest) {
+        			var destBody = JSON.parse(body);
+              if(destBody && destBody.stations && destBody.stations.length >= 1) {
+                var dest = destBody.stations[0].code;
                 destStationCode = dest;
                 if(srcStationCode && destStationCode && date) {
                   var formattedDate = moment(date).format('DD-MM-YYYY');
@@ -41,13 +43,14 @@ server.post('/IRCTC-find-trains', (req, res) => {
                 			console.log("ERR:", error);
                 		} else {
                       var trains = JSON.parse(body).trains;
-                      if(trains) {
+                      if(trains && trains.length >= 1) {
                         var trainsList = [];
                   			for (var i = 0; i < trains.length; i++) {
                           var replacedStr = trains[i].name.replace(/EXP/g, "Express");
                           var trainInfo = replacedStr.toLowerCase() + ' at ' + trains[i].src_departure_time;
                   				trainsList.push(trainInfo);
                   			}
+
                         let dataToSend = "Available trains are " + trainsList.toString();
 
                         return res.json({
