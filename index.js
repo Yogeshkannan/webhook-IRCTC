df59207f1877fa2b74ae67807a85492e2119dd2a
Yogesh <yogesh@10decoders.in>
2018-04-25 14:23:38 +0530

    Error handling

--- index.js
+++ index.js
@@ -24,54 +24,70 @@ server.post('/IRCTC-find-trains', (req, res) => {
   			console.log("ERR:", error);
   		} else {
   			var src = JSON.parse(body).stations[0].code;
-        srcStationCode = src;
-        request('https://api.railwayapi.com/v2/name-to-code/station/'+destination+'/apikey/'+apiKey+'/', function(error, responseFromAPI, body) {
-      		if (error) {
-      			console.log("ERR:", error);
-      		} else {
-      			var dest = JSON.parse(body).stations[0].code;
-            destStationCode = dest;
-            if(srcStationCode && destStationCode && date) {
-              var formattedDate = moment(date).format('DD-MM-YYYY');
-              const reqUrl = 'https://api.railwayapi.com/v2/between/source/'+srcStationCode+'/dest/'+destStationCode+'/date/'+formattedDate+'/apikey/'+apiKey+'/';
-              request(reqUrl, function(error, responseFromAPI, body) {
-            		if (error) {
-            			console.log("ERR:", error);
-            		} else {
-                  var trains = JSON.parse(body).trains;
-                  if(trains) {
-                    var trainsList = [];
-              			for (var i = 0; i < trains.length; i++) {
-                      var replacedStr = trains[i].name.replace(/EXP/g, "Express");
-                      var trainInfo = replacedStr.toLowerCase() + ' at ' + trains[i].src_departure_time;
-              				trainsList.push(trainInfo);
-              			}
-                    let dataToSend = "Available trains are " + trainsList.toString();
+        if(src) {
+          srcStationCode = src;
+          request('https://api.railwayapi.com/v2/name-to-code/station/'+destination+'/apikey/'+apiKey+'/', function(error, responseFromAPI, body) {
+        		if (error) {
+        			console.log("ERR:", error);
+        		} else {
+        			var dest = JSON.parse(body).stations[0].code;
+              if(dest) {
+                destStationCode = dest;
+                if(srcStationCode && destStationCode && date) {
+                  var formattedDate = moment(date).format('DD-MM-YYYY');
+                  const reqUrl = 'https://api.railwayapi.com/v2/between/source/'+srcStationCode+'/dest/'+destStationCode+'/date/'+formattedDate+'/apikey/'+apiKey+'/';
+                  request(reqUrl, function(error, responseFromAPI, body) {
+                		if (error) {
+                			console.log("ERR:", error);
+                		} else {
+                      var trains = JSON.parse(body).trains;
+                      if(trains) {
+                        var trainsList = [];
+                  			for (var i = 0; i < trains.length; i++) {
+                          var replacedStr = trains[i].name.replace(/EXP/g, "Express");
+                          var trainInfo = replacedStr.toLowerCase() + ' at ' + trains[i].src_departure_time;
+                  				trainsList.push(trainInfo);
+                  			}
+                        let dataToSend = "Available trains are " + trainsList.toString();
 
-                    return res.json({
-                        speech: dataToSend,
-                        displayText: dataToSend,
-                        source: 'IRCTC-find-trains'
-                    });
-                  } else {
-                    return res.json({
-                        speech: 'No trains are available',
-                        displayText: 'No trains are available',
-                        source: 'IRCTC-find-trains'
-                    });
-                  }
+                        return res.json({
+                            speech: dataToSend,
+                            displayText: dataToSend,
+                            source: 'IRCTC-find-trains'
+                        });
+                      } else {
+                        return res.json({
+                            speech: 'No trains are available',
+                            displayText: 'No trains are available',
+                            source: 'IRCTC-find-trains'
+                        });
+                      }
 
-            		}
-              });
-            } else {
-              return res.json({
-                  speech: 'Invalid information',
-                  displayText: 'Invalid information',
-                  source: 'IRCTC'
-              });
-            }
-      		}
-        });
+                		}
+                  });
+                } else {
+                  return res.json({
+                      speech: 'Invalid information',
+                      displayText: 'Invalid information',
+                      source: 'IRCTC-find-trains'
+                  });
+                }
+              } else {
+                return res.json({
+                    speech: 'Invalid destination name',
+                    displayText: 'Invalid destination name',
+                    source: 'IRCTC-find-trains'
+                });
+              }
+        		}
+          });
+        } else {
+          return res.json({
+              speech: 'Invalid source name',
+              displayText: 'Invalid source name',
+              source: 'IRCTC-find-trains'
+          });
+        }
   		}
     });
 });
